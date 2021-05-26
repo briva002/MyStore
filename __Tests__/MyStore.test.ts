c9810a91aa313e06ab2f4e6caab39ad74a6a0611
briva002 <79668462+briva002@users.noreply.github.com>
2021-05-26 13:58:49 -0700

    Mystore2

--- __Tests__/MyStore.test.ts
+++ __Tests__/MyStore.test.ts
@@ -12,4 +12,32 @@ await myPage.click(myPage.loginbutton)
 await myPage.setInput(myPage.emailaddress,'Belinda.Rivas1@gmail.com')
 await myPage.setInput(myPage.password, 'Miami305!')
 await myPage.click(myPage.submitbutton)
+
 })
+
+test('adds items to cart', async () => {
+await myPage.navigate()
+await myPage.click(myPage.womens)
+await myPage.click(myPage.addtocartbutton)
+await myPage.click(myPage.shoppingcartbutton)
+
+})
+
+test('User can view order history', async () => {
+    await myPage.navigate()
+    await myPage.click(myPage.account)
+    await myPage.click(myPage.orderhistory)
+    await myDriver.sleep(2000)
+      
+})
+test('User can fill out contact page', async () => {
+    await myPage.navigate()
+    await myPage.click(myPage.contactus)
+    await myPage.click(myPage.subjectheading)
+    await myPage.setInput(myPage.message, 'Your store is awesome')
+    await myPage.click(myPage.submitmessage)
+})
+afterAll(async () => {
+    await myDriver.quit();
+});
+
--- pageObjects/myStore.ts
+++ pageObjects/myStore.ts
@@ -6,9 +6,23 @@ export class myStore extends BasePage {
         super(driver, url)
     }
     emailaddress: By = By.id('email')
+
+    emailcreate: By = By.name('email_create')
+    submitnewemail:By = By.id('SubmitCreate')
     password: By = By.id('passwd')
     loginbutton:By = By.className('login')
     emailinput: By = By.className('is_required validate account_input form-control')
     submitbutton: By = By.className('icon-lock left')
+    addtocartbutton: By = By.className('button ajax_add_to_cart_button btn btn-default')
+    proceedtocheckout: By = By.css('i[class="standard-checkout button"]')
+    shoppingcartbutton: By = By.className('shopping_cart')
+    womens: By = By.className('sf-with-ul')
+    account: By = By.className('account')
+    orderhistory: By = By.className('icon-list-ol')
+    contactus: By = By.id('contact-link')
+    message: By = By.id('message')
+    submitmessage: By = By.id("submitMessage")
+    subjectheading: By = By.id('id_contact')
     
+
 }
